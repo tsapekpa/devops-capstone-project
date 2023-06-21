9a28dc19b3860dcdee000e6089dde535bf4c8178
tsapekpa <tsapekpa@gmail.com>
2023-06-21 05:08:44 -0400

    another update for lint errors

--- service/routes.py
+++ service/routes.py
@@ -84,7 +84,7 @@ def get_accounts(account_id):
     This endpoint will read an Account based the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
-    account = Account.find(account_id)    
+    account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
@@ -107,7 +107,7 @@ def update_accounts(account_id):
     account.update()
     return account.serialize(), status.HTTP_200_OK
 
-    
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
